9313d0a8ac9e3ac6ff18a31a2e6e5f2d78a9956c
Eternity1001 <MMirhossain01@gmail.com>
2024-02-16 17:59:49 -0500

    Hard coded login and sign for testing purpose until I figure out how to connect to database

---
+++ PHPBackEnd/functionLoginIn.php
@@ -0,0 +1,26 @@
+<?php
+
+if (isset($_POST["LoginButton"])){
+
+    $existingAccount = "testAcc";
+    $existingPassword = "test123";
+    
+    $name = $_POST["username"];
+    $password = $_POST["pass"];
+
+    if (($existingAccount == $name) & ($existingPassword == $password)){
+        header("location: /student_home.php");
+    }
+    else{
+        echo "Account Doesn't exist";
+    }
+    
+}
+
+if (isset($_POST["signup"])){
+
+    header("location: /signup.php");
+
+
+
+}
