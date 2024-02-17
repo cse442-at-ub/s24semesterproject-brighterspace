2edb6de8ef56b2edf19e26e9b51b10f310a06567
Eternity1001 <MMirhossain01@gmail.com>
2024-02-16 19:08:39 -0500

    HardCoded logining and signup

--- PHPBackEnd/functionLoginIn.php
+++ PHPBackEnd/functionLoginIn.php
@@ -8,16 +8,19 @@ if (isset($_POST["LoginButton"])){
     $name = $_POST["username"];
     $password = $_POST["pass"];
 
+
     if (($existingAccount == $name) & ($existingPassword == $password)){
+       
         header("location: /student_home.php");
     }
+
     else{
         echo "Account Doesn't exist";
     }
     
 }
 
-if (isset($_POST["signup"])){
+if (isset($_POST["signUp"])){
 
     header("location: /signup.php");
 
--- login.php
+++ login.php
@@ -11,11 +11,11 @@
 <a>replace with logo</a>
 
 <hr>
-<form action = "PHPBackEnd\functionLoginIn.php" method = "post"> 
+<form action = 'PHPBackEnd\functionLoginIn.php' method = "post"> 
 <table>
   <tr>
     <td><label for="username"><b>Username</b></label></td>
-	<td><input type="text" placeholder="Enter Username" name="username"  required></td>
+	<td><input type="text" placeholder="Enter Username" name="username" required></td>
   </tr>
   <tr>
     <td><label for="pass"><b>Password</b></label></td>
@@ -23,10 +23,12 @@
   </tr>
 </table>
 <button type="submit" name = "LoginButton">Login</button>
-<button type ="submit" name = "signup"> Sign Up</button>
+<button type="submit" name = "signUp">signup</button>
+
+</form>
 <br>
 <a href="password reset page here">forgot your password?</a>
-</form>
+
 <hr>
 
 
