442f9e59aea2ee759c60c2424c9126a61db70f25
donut1000 <donut10004085@gmail.com>
2024-02-17 15:15:05 -0500

    Completed logout.php and adding it to Github branch

---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+//Clearing the user's cookie
+setcookie('username','testuser',-10);
+
+//([VARIABLE FOR USERNAME],[THE USERNAME], [TIME])
+
+//Kills all the variables within the session and cleans up the cookies
+$_SESSION=array();
+
+#Destorys the session
+session_destroy();
+
+//Redirects to the login page
+header('location:FunctionLogin.php');
+
