6a35a2409c7dd7c30b5a4221e967691a4372f5ab
BrandonChen76 <bchen76@buffalo.edu>
2024-02-17 19:21:00 -0500

    merging aaaaaaaaaaaaaaa so frustrating

--- signup.php
+++ signup.php
@@ -1,40 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Sign Up</title>
-<meta charset="UTF-8">
-</head>
-<body>
+<?php 
 
-<div class="container">
+if (isset($_POST["submit"])) {
 
-<a>replace with logo</a>
-<hr>
-<table>
-  <tr>
-    <td><label for="name"><b>Name</b></label></td>
-	<td><input type="text" placeholder="Enter Name" name="username" required></td>
-  </tr>
-  <tr>
-    <td><label for="username"><b>Username</b></label></td>
-	<td><input type="text" placeholder="Enter Username" name="name" required></td>
-  </tr>
-  <tr>
-    <td><label for="email"><b>Email</b></label></td>
-	<td><input type="email" placeholder="Enter Email" name="uemail" required></td>
-  </tr>
-  <tr>
-    <td><label for="psw"><b>Password</b></label></td>
-	<td><input type="password" placeholder="Enter Password" name="pass" required></td>
-  </tr>
-  <tr>
-    <td><label for="psw"><b>Confirm Password</b></label></td>
-	<td><input type="password" placeholder="Enter Password" name="cpass" required></td>
-  </tr>
-</table>
-<button type="submit">Create</button>
-<hr>
+    $name = $_POST["name"];
+    $username = $_POST["Username"];
+    $email = $_POST["email"];
+    $password = $_POST["pass"];
 
-</div>
-</body>
-</html>
+    
+    header("location: /signIn.php");
+}
+else{
+   echo "failure";
+}
