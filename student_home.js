de7cad3eae9693124465cf3146e0e46d19b2d7be
BrandonChen76 <bchen76@buffalo.edu>
2024-02-23 16:52:32 -0500

    Fixed the navbar elements to be displayed correctly.

--- student_home.js
+++ student_home.js
@@ -11,7 +11,7 @@ var studentID = "90807060";
 
 //append class to list in Div Classes
 var ulElementClasses = document.querySelector(".classes ul");
-classNames.forEach(function(currClass) {
+classNames.forEach(function(currClass) { //change this
     var liElementClasses = document.createElement("li");
     var aElementClasses = document.createElement("a");
     aElementClasses.href = currClass + ".html";
@@ -21,16 +21,18 @@ classNames.forEach(function(currClass) {
 });
 
 //append class to list in Div Navbar
-var ulElementNavbar = document.querySelector(".nabar ul");
+var ulElementNavbar = document.querySelector(".navbar ul");
 var liElementName = document.createElement("li");
 var aElementName = document.createElement("a");
-liElementName.appendChild(aElementClasses);
-ulElementName.appendChild(liElementClasses);
+aElementName.textContent = "Name: " + studentName; //change this
+liElementName.appendChild(aElementName);
+ulElementNavbar.appendChild(liElementName);
 
 var liElementID = document.createElement("li");
 var aElementID = document.createElement("a");
-liElementID.appendChild(aElementClasses);
-ulElementID.appendChild(liElementClasses);
+aElementID.textContent = "Student ID: " + studentID; //change this
+liElementID.appendChild(aElementID);
+ulElementNavbar.appendChild(liElementID);
 
 
 function toggletab() {
