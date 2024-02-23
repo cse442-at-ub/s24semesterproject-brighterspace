1fd66d64a8df373b721fa27b4a360552cd566450
Eternity1001 <112906683+Eternity1001@users.noreply.github.com>
2024-02-23 18:31:05 -0500

    Added Admin Login and Cookies

---
+++ PHPBackEnd/adminLoginIn.php
@@ -0,0 +1,60 @@
+<?php
+
+require "dbConnection.php";
+
+if (isset($_POST["LoginButton"])) {
+
+
+    $conn = database();
+    $name = $_POST["username"];
+    $password = $_POST["pass"];
+
+    $rows = readDataBase($name);
+    if ($rows != null){
+        echo "working";
+
+        if (password_verify($password,$rows[1]) === true){
+            session_start();
+            setcookie('user', json_encode([
+                'username' => $rows[0],
+                'password' => $rows[1]
+            ]), time() + 3600 * 24 *30);
+            header("location: \brighterspace\student_home.php");
+
+        }
+    }
+}
+
+function addAdminUser($name, $password, $email){
+
+    $conn = database();
+    $hashed = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO LoginInfo (Name, Password, Email, AdminStatus) VALUES ('" . $name . "','" . $hashed . "', '" . $email . "', '1' )";
+
+    if ($conn->query($sql)){
+        echo "It added";
+    }
+    else {
+        echo "Error has Occured";
+    }
+
+
+}
+
+function readDataBase($name){
+    $conn = database();
+
+    $sql = "SELECT * FROM LoginInfo";
+    $results = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($results) > 0){
+
+        while ($row = mysqli_fetch_row($results)){
+            if ($row[0] == $name){
+                return $row;
+            }
+        }
+    }
+    return null;
+}
