55581643ca21a0a578c1a72522aa470e18974e55
Eternity1001 <112906683+Eternity1001@users.noreply.github.com>
2024-02-23 19:29:54 -0500

    Signing up now stores on the Server

--- PHPBackEnd/functionSignUp.php
+++ PHPBackEnd/functionSignUp.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
+
+require "dbConnection.php";
+
 if (isset($_POST["submit"])){
 
     $name = $_POST["name"];
@@ -6,8 +9,19 @@ if (isset($_POST["submit"])){
     $uemail = $_POST["uemail"];
     $pass = $_POST["pass"];
 
+    $conn = database();
+    
+    $hashed = password_hash($pass, PASSWORD_DEFAULT);
 
+    $sql = "INSERT INTO AccountInfo (Name, Username, Email, Password, Admin) VALUES ('{$name}', '{$username}', '{$uemail}','$hashed', '0')";
 
+    if ($conn->query($sql)){
+        echo "It added";
+    }
+    else {
+        echo "Error has Occurred";
+    }
+    
     header("location: /login.php");
 
 }
