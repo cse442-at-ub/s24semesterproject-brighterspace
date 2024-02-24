b9560e1f54ec81bad675067b953acc57d5b92027
Eternity1001 <112906683+Eternity1001@users.noreply.github.com>
2024-02-23 19:21:54 -0500

    Update adminLoginIn.php to work with current file

--- PHPBackEnd/adminLoginIn.php
+++ PHPBackEnd/adminLoginIn.php
@@ -4,6 +4,10 @@ require "dbConnection.php";
 
 if (isset($_POST["LoginButton"])) {
 
+    # Check if we found the user. If we did, we verify their password
+    # If their password is correct, we redirect them and provide a http response
+    # The http response contain admin which let them know if the user is admin.
+    # 0 = Not Admin, 1 = Admin
 
     $conn = database();
     $name = $_POST["username"];
@@ -13,11 +17,11 @@ if (isset($_POST["LoginButton"])) {
     if ($rows != null){
         echo "working";
 
-        if (password_verify($password,$rows[1]) === true){
+        if (password_verify($password,$rows[3]) === true){
             session_start();
             setcookie('user', json_encode([
-                'username' => $rows[0],
-                'password' => $rows[1]
+                'username' => $rows[1],
+                'password' => $rows[3]
             ]), time() + 3600 * 24 *30);
 
             $content = $rows[3];
@@ -26,16 +30,19 @@ if (isset($_POST["LoginButton"])) {
             header("AdminStatus: {$content}");
             header("X-Content-Type-Options: nosnifff");
             header("location: \brighterspace\student_home.php");
+
         }
     }
 }
 
-function addAdminUser($name, $password, $email){
+function addAdminUser($name,$username, $password, $email){
+    
+    # Allows the team to create a admin account on our database so the admin can Login in once we provide the info
 
     $conn = database();
     $hashed = password_hash($password, PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO LoginInfo (Name, Password, Email, AdminStatus) VALUES ('" . $name . "','" . $hashed . "', '" . $email . "', '1' )";
+    $sql = "INSERT INTO AccountInfo (Name, Username, Email, Password, Admin) VALUES ('{$name}', '{$username}', '{$email}','$hashed', '1')";
 
     if ($conn->query($sql)){
         echo "It added";
@@ -50,16 +57,19 @@ function addAdminUser($name, $password, $email){
 function readDataBase($name){
     $conn = database();
 
-    $sql = "SELECT * FROM LoginInfo";
+    $sql = "SELECT * FROM AccountInfo";
     $results = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($results) > 0){
 
         while ($row = mysqli_fetch_row($results)){
-            if ($row[0] == $name){
+            if ($row[1] == $name){
                 return $row;
             }
         }
     }
     return null;
 }
+
+
+
