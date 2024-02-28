963b6fd498427a8ecc41347698e3d393a2d7ecea
Eternity1001 <112906683+Eternity1001@users.noreply.github.com>
2024-02-28 11:47:17 -0500

    Removed Duplicate functions Card:34

--- PHPBackEnd/dbConnection.php
+++ PHPBackEnd/dbConnection.php
@@ -16,22 +16,4 @@ function database(){
 
     global $conn;
     return $conn;
-}<?php
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'test';
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn ->connect_error){
-
-    die("Connection Failed: ".$conn ->connect_error);
-}
-
-function database(){
-
-    global $conn;
-    return $conn;
-}
