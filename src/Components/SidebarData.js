c42ee8cade873260e8163748b3ead070b5f287b0
donut1000 <ajfeng@buffalo.edu>
2024-03-05 17:19:28 -0500

    Progress on redoing the navbar

---
+++ src/Components/SidebarData.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+import * as IoIcons from 'react-icons/io'
+import * as RiIcons from 'react-icons/ri'
+
+export const SidebarData = [
+    {
+        title: 'Overview',
+        path: '/overview',
+        icon: <AiIcons.AiFillHome />
+        iconClosed: <RiIcons.RiArrowDownSFill/>
+
+    }
+]
