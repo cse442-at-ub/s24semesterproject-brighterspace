0756f266a986a9c7bca3dc21c8e144c82b07bbc6
donut1000 <ajfeng@buffalo.edu>
2024-03-09 15:15:10 -0500

    Adjusting app.js on the ReactApp #40
    Task#40
    Adjusting app.js on the ReactApp

--- brighterspace-react-app/src/App.js
+++ brighterspace-react-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+//import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
 import Login from './pages/login'
 import StudentLogin from './pages/student-login'
@@ -7,13 +7,19 @@ import StudentHome from './pages/StudentHome'
 import './App.css'
 import TeacherAddClass from './pages/TeacherAddClass'
 import ExampleClassPage from './pages/ExampleClassPage'
+import Sidebar from "./components/Sidebar";
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Overview from './pages/Overview'
+import {Classes, ClassOne, ClassTwo, ClassThree} from './pages/Classes'
+import Calendar from './pages/Calendar'
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
+        <Router>
+          <Sidebar/>
           <Routes>
             <Route path="/" element={<Home  />} />
             <Route path="/login" element={<Login  />} />
@@ -22,8 +28,14 @@ function App() {
             <Route path="/student-home" element={<StudentHome  />} />
             <Route path="/teacher-home/add-class" element={<TeacherAddClass  />} />
             <Route path="/class/:classId" element={<ExampleClassPage  />} />
+            <Route path="/overview" element={<Overview/>} />
+            <Route path="/classes" element={<Classes/>} />
+            <Route path="/classes/class1" element={<ClassOne/>} />
+            <Route path="/classes/class2" element={<ClassTwo/>} />
+            <Route path="/classes/class3" element={<ClassThree/>} />
+            <Route path="/calendar" element={<Calendar/>} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </header>
     </div>
   )
