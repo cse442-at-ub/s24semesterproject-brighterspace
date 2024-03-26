d56fe014d18b4d7d2dd20446f7aa7a6ca624282d
BrandonChen76 <bchen76@buffalo.edu>
2024-03-25 21:57:05 -0400

    just testing hashrouter

--- brighterspace-react-app/src/App.js
+++ brighterspace-react-app/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {HashRouter as Router,Routes,Route} from 'react-router-dom'
 //import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
 import Login from './pages/login'
