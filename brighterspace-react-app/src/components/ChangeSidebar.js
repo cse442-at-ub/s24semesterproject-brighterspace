03385507345c732bff7201cd4c7ba0970c0de735
donut1000 <ajfeng@buffalo.edu>
2024-04-03 20:47:15 -0400

    Fixed navbar to hide at login #68
    Task#68
    Made a new component to hide navbar at login pages

--- brighterspace-react-app/src/components/ChangeSidebar.js
+++ brighterspace-react-app/src/components/ChangeSidebar.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import {useLocation} from 'react-router-dom'
+import {useEffect,useState} from 'react'
 
 const ChangeSidebar = ({children}) => {
+    const location = useLocation();
+    const [showsidebar,setshowsidebar] = useState(false)
+
+    useEffect(()=> {
+        console.log("this is location",location)
+        if(location.pathname == '/login'){
+            setshowsidebar(false)
+
+        }
+
+        else if(location.pathname == '/student-login'){
+            setshowsidebar(false)
+
+        }
+        else if(location.pathname == '/admin-login'){
+            setshowsidebar(false)
+
+        }
+        else{
+            setshowsidebar(true)
+        }
+
+    },[location])
     return (
-        <div>{children}</div>
+        <div>{showsidebar && children}</div>
     )
 }
 export default ChangeSidebar
