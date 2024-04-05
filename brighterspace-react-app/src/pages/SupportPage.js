f32ba7802a022fc553a419d68e410b2e3027edb2
donut1000 <ajfeng@buffalo.edu>
2024-04-05 13:43:54 -0400

    Added user feedback #54
    Task#54
    Added user feedback on the new branch to confirm the submission of support tickets

--- brighterspace-react-app/src/pages/SupportPage.js
+++ brighterspace-react-app/src/pages/SupportPage.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import '../styles/SupportPage.css'
 
 const SupportPage = () => {
+    const [support,setSupport] = useState("");
+    const onChangeHandler = (e) => {
+        setSupport(e.target.value)
+    };
     return (
-        <div>SupportPage</div>
+        <div className="main_container">
+            <div className="support_container">{support}</div>
+            <div className="support_box">
+            <h3 className="support_text">Submit your support ticket</h3>
+            <textarea
+                value={support}
+                onChange={onChangeHandler}
+                className="input_box"/>
+            <button className="ticket_button">Submit Ticket</button>
+            </div>
+        </div>
     )
 }
 export default SupportPage
