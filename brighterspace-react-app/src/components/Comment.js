ef76ef3cb9da8961558e6ce73c4a778a3fe493ef
donut1000 <ajfeng@buffalo.edu>
2024-04-15 14:05:58 -0400

    Built functionality of discussion board #61
    Task#61
    Built the functionality of the discussion board, resolving a few bugs

--- brighterspace-react-app/src/components/Comment.js
+++ brighterspace-react-app/src/components/Comment.js
@@ -1,18 +1,52 @@
 import "../styles/Comment.css"
+import { useState } from "react"
 
-const Comment = ({ comments }) => {
+const Comment = ({ comments, handleAddComments, handleDeleteComments }) => {
+    const [showInput,setShowInput]  = useState(false)
+    const [commentBody,setCommentBody] = useState("")
+
+    const handleAdd = () => {
+        let newComments = {
+            id:Date.now(),
+            text:commentBody,
+            replies: [],
+
+        }
+        handleAddComments(comments.id,newComments)
+        setShowInput(false)
+    }
     return(
         <div>
             <div className="comment-container">
                 <h3>{comments.text}</h3>
-                <div>
-                    <button>Reply</button>
-                    <button>Delete</button>
-                </div>
+                {showInput && <input
+                    type="text"
+                    autoFocus
+                    onChange={(e)=>setCommentBody(e.target.value)}
+                />}
+                {
+                    showInput? (
+                        <div>
+                            <button onClick={handleAdd}>Add</button>
+                            <button onClick={()=>setShowInput(false)}>Cancel</button>
+                        </div>
+                    ): (
+                        <div>
+                            <button onClick={() => setShowInput(true)}>Reply</button>
+                            <button onClick>Delete</button>
+                        </div>
+                    )
+                }
+
             </div>
-            <div>
+            <div style={{paddingLeft: 25}}>
                 {comments?.replies?.map((ele)=> (
-                    <Comment key={comments.id} comments={ele}/>
+                    <Comment
+                        key={ele.id}
+                        comments={ele}
+                        handleAddComments={handleAddComments}
+                        handleDeleteComments={handleDeleteComments}
+                    />
                 ))}
             </div>
         </div>
--- brighterspace-react-app/src/pages/DiscussionBoard.js
+++ brighterspace-react-app/src/pages/DiscussionBoard.js
@@ -2,14 +2,30 @@ import React, {useState} from 'react'
 import Comment from "../components/Comment"
 import {CommentData} from "../components/CommentData"
 import "../styles/DiscussionBoard.css"
+import useFunctions from "../components/UseFunctions"
 
 
 
 export default function DiscussionBoard(){
     const [comments, setComments] = useState(CommentData);
+    const { addComment,deleteComment } = useFunctions()
+    const handleAddComments = (commentId,comment) => {
+        const updatedTree = addComment(comments,commentId,comment)
+        setComments(updatedTree)
+
+    }
+    const handleDeleteComments = (commentId) => {
+        const updatedTree = deleteCommment(comments, commentId)
+        setComments(updatedTree)
+    }
     return(
         <div className="comment-app">
-            <Comment comments={comments}/>
+            <Comment
+                key={comments.id}
+                comments={comments}
+                handleAddComments={handleAddComments}
+                handleDeleteComments={handleDeleteComments}
+            />
         </div>
     );
 }
