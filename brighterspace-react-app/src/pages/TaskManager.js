32e509e84081d9900673e32e6f948028664a15e5
Eternity1001 <MMirhossain01@gmail.com>
2024-04-17 10:34:24 -0400

    Added the ability to save task between session. Delete not working yet

--- brighterspace-react-app/src/pages/TaskManager.js
+++ brighterspace-react-app/src/pages/TaskManager.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import * as FaIcons from 'react-icons/fa'
 import * as AiIcons from 'react-icons/ai'
 import * as IoIcons from 'react-icons/io'
@@ -6,42 +6,101 @@ import * as RiIcons from 'react-icons/ri'
 import * as Fa6 from "react-icons/fa6";
 import "../styles/TaskManager.css"
 
+
 function TaskManager(){
     const[tasks,setTasks] = useState([]);
     const[newTask, setNewTask] = useState("");
 
+
+
+    const update_task = () => {
+        const request = new XMLHttpRequest();
+
+        request.open("POST", "http://localhost/s24semesterproject-brighterspace/PHPBackEnd/taskManger.php");
+        const credentialsJSON = {"Update": 'Update'};
+        update_messages(request, credentialsJSON)
+    }
+    useEffect(() => {
+        // Call the function when the component mounts
+        update_task();
+    }, []);
+
+    function update_messages(request, credentialsJSON){
+
+        request.onload = () => {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    const responseText = request.responseText;
+                    const task_data = JSON.parse(responseText)
+
+
+                    const messages = []
+
+                    for (const key in task_data) {
+                        if (task_data.hasOwnProperty(key)) {
+                            const value = task_data[key];
+                            messages.push(value)
+                        }
+                    }
+                    setTasks(messages)
+                    setNewTask("")
+                } else {
+                    setNewTask("")
+                    console.error("Error:", request.status, request.statusText);
+                }
+            }
+        };
+        request.send(JSON.stringify(credentialsJSON));
+    }
     function handleInputChange(event){
         setNewTask(event.target.value)
 
-    }
 
+    }
     function addTask(){
+        const request = new XMLHttpRequest();
         if(newTask.trim() !== "") {
-            setTasks(t => [...t, newTask])
-            setNewTask("")
+
+            const credentialsJSON = {"Ticket": newTask.valueOf()};
+
+            //TODO update path for server
+            request.open("POST", "http://localhost/s24semesterproject-brighterspace/PHPBackEnd/taskManger.php");
+            update_messages(request, credentialsJSON)
         }
 
 
 
     }
     function deleteTask(index){
-        const updatedTasks = tasks.filter((_,i)=> i !== index)
-        setTasks(updatedTasks);
+        const request = new XMLHttpRequest();
+        const credentialsJSON = {"delete": index};
+        request.open("POST", "http://localhost/s24semesterproject-brighterspace/PHPBackEnd/taskManger.php");
+        update_messages(request, credentialsJSON)
+
 
     }
     function moveTaskUp(index){
         if(index>0){
-            const updatedTasks = [...tasks];
-            [updatedTasks[index],updatedTasks[index-1]] = [updatedTasks[index-1],updatedTasks[index]]
-            setTasks(updatedTasks)
+            const request = new XMLHttpRequest();
+            const credentialsJSON = {"addPriority": index};
+            request.open("POST", "http://localhost/s24semesterproject-brighterspace/PHPBackEnd/taskManger.php");
+
+            // const updatedTasks = [...tasks];
+
+            update_messages(request, credentialsJSON);
+            //     [updatedTasks[index],updatedTasks[index-1]] = [updatedTasks[index-1],updatedTasks[index]]
+            // setTasks(updatedTasks)
         }
 
     }
     function moveTaskDown(index){
         if(index<tasks.length-1){
+            const request = new XMLHttpRequest();
+            const credentialsJSON = {"subPriority": index};
+            request.open("POST", "http://localhost/s24semesterproject-brighterspace/PHPBackEnd/taskManger.php");
             const updatedTasks = [...tasks];
-            [updatedTasks[index],updatedTasks[index+1]] = [updatedTasks[index+1],updatedTasks[index]]
-            setTasks(updatedTasks)
+            update_messages(request,credentialsJSON);
+
         }
     }
 
