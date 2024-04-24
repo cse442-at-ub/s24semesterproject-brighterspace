423b4e889ff4ff0b32c1156916842224b37fd804
Taran-Chana <124089978+Taran-Chana@users.noreply.github.com>
2024-04-24 17:01:57 -0400

    Update addGrade.php
    fixed backend for addgrade so grades are stored as 1 long string per student

--- PHPBackEnd/addGrade.php
+++ PHPBackEnd/addGrade.php
@@ -7,7 +7,7 @@ header('SameSite=None');
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-    isAllowed();
+    attempt();
 }
 
 
@@ -19,6 +19,7 @@ function getInfo() :array
     $assignment = '';
     $name = '';
     $points = '';
+    $max = '';
     foreach ($body as $key => $value) {
         if ($key == 'class') {
             $class = $value;
@@ -29,30 +30,74 @@ function getInfo() :array
         elseif ($key == 'student'){
             $name = $value;
         }
+        elseif ($key =='max'){
+            $max = $value;
+        }
         else {
             $points = $value;
         }
     }
-    return [$class, $assignment, $name, $points];
+    return [$class, $assignment, $name, $points, $max];
 }
 
-function isAllowed()
+function attempt()
 {
     $info = getInfo();
     $class = $info[0];
     $assignment = $info[1];
     $name = $info[2];
     $points = $info[3];
-
-    setMaxPoints($class, $assignment, $name, $points);
+    $max = $info[4];
+    addAssignment($class, $assignment, $name, $points, $max);
 }
 
 
-function setMaxPoints($class, $assignment, $name, $points): bool
+function addAssignment($class, $assignment, $name, $points, $max)
+{
+
+    $array = array(
+        "assignment_name" => $assignment,
+        "score" => $points . "/" . $max,
+    );
+    $atext = $assignment . ":" . $points . "/" . $max;
+
+    $isinDB = inDB($name, $class);
+    if($isinDB !== ""){
+        $isinDB =  $isinDB . "&". $atext;
+        echo($isinDB);
+        $conn = database();
+        $sql = "UPDATE `$class` SET Assignments='{$isinDB}' WHERE Student='{$name}'";
+        if ($conn->query($sql) === TRUE) {
+            return true;
+        } else {
+            return false;
+        }
+    } else{
+        addStudent($class, $name, $atext);
+    }
+
+}
+
+function inDB($name, $class) : string
 {
     $conn = database();
-    $sql = "UPDATE '$class' SET '$assignment'='$points' WHERE Student='$name'";
-    if ($conn->query($sql) === TRUE) {
+    $sql = "SELECT * FROM `$class`;";
+    $results = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($results) > 0) {
+        while ($row = mysqli_fetch_row($results)) {
+            if ($row[0] == $name) {
+                return $row[1];
+            }
+        }
+    }
+    return "";
+}
+
+function addStudent($class, $name, $atext): bool
+{
+    $conn = database();
+    $sql = "INSERT INTO `$class`(`Student`, `Assignments`) VALUES ('{$name}','{$atext}')";
+    if ($conn->query($sql)) {
         return true;
     } else {
         return false;
