88419e464a4332498e227e2414e9425be89c5425
Brookz-180 <jjbrooks180@gmail.com>
2024-04-26 21:03:28 -0400

    removed sending response from php video upload

--- PHPBackEnd/videoUpload.php
+++ PHPBackEnd/videoUpload.php
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
                 ];
             }
         header('Content-Type: application/json');
-        echo json_encode($response);
+        // echo json_encode($response);
         }
     }
 } else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
--- brighterspace-react-app/src/components/TeacherUploadRec/TeacherUploadRec.jsx
+++ brighterspace-react-app/src/components/TeacherUploadRec/TeacherUploadRec.jsx
@@ -39,12 +39,12 @@ export default function TeacherUploadRec() {
             method: 'POST',
             body: formData
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
+        // .then(response => {
+        //     if (!response.ok) {
+        //         throw new Error('Network response was not ok');
+        //     }
+        //     return response.json();
+        // })
         .catch(error => {
             console.error("problem sending video post", error);
         });
